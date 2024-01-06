49b48fd2c85319ee9bd1ef1ce64d416cc88ce9b3
Vladimir Solovyov <svn131131@gmail.com>
2024-01-06 17:01:45 +0500

    2

---
+++ src/main/java/com/example/translator/JavaCodeAnalyzerWithJavaParser.java
@@ -0,0 +1,88 @@
+package com.example.translator;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParserConfiguration;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
+
+public class JavaCodeAnalyzerWithJavaParser {
+
+    private Set<String> packages = new HashSet<>();
+    private Set<String> classes = new HashSet<>();
+    private Set<String> methods = new HashSet<>();
+    private Set<String> variables = new HashSet<>();
+
+    public void analyzeFile(String filePath) throws Exception {
+        FileInputStream in = new FileInputStream(filePath);
+
+        // Configure the parser
+        ParserConfiguration configuration = new ParserConfiguration();
+        JavaParser parser = new JavaParser(configuration);
+
+        // Parse the file
+        CompilationUnit cu = parser.parse(in).getResult().orElseThrow(() -> new IllegalArgumentException("Unable to parse the file."));
+
+        // Visit class names
+        cu.accept(new VoidVisitorAdapter<Void>() {
+            @Override
+            public void visit(ClassOrInterfaceDeclaration n, Void arg) {
+                classes.add(n.getNameAsString());
+                super.visit(n, arg);
+            }
+        }, null);
+
+        // Visit method names
+        cu.accept(new VoidVisitorAdapter<Void>() {
+            @Override
+            public void visit(MethodDeclaration n, Void arg) {
+                methods.add(n.getNameAsString());
+                super.visit(n, arg);
+            }
+        }, null);
+
+        // Visit variable names
+        cu.accept(new VoidVisitorAdapter<Void>() {
+            @Override
+            public void visit(VariableDeclarationExpr n, Void arg) {
+                n.getVariables().forEach(var -> {
+                    if (var.getNameAsString().length() > 1) { // Exclude single-letter variables
+                        variables.add(var.getNameAsString());
+                    }
+                });
+                super.visit(n, arg);
+            }
+        }, null);
+
+        // If the file has a package declaration, add it
+        cu.getPackageDeclaration().ifPresent(pd -> packages.add(pd.getNameAsString()));
+
+        // Write results to files
+        writeSetToFile(packages, "packages.txt");
+        writeSetToFile(classes, "classes.txt");
+        writeSetToFile(methods, "methods.txt");
+        writeSetToFile(variables, "variables.txt");
+
+        in.close();
+    }
+
+    private void writeSetToFile(Set<String> set, String fileName) throws Exception {
+        Files.write(Paths.get(fileName), set);
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Usage: JavaCodeAnalyzerWithJavaParser <file-to-analyze>");
+            return;
+        }
+        new JavaCodeAnalyzerWithJavaParser().analyzeFile(args[0]);
+    }
+}
